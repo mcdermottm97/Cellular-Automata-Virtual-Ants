49d8762008c8c7ec6068829959c02b1fe3d23cb6
mcdermottm97 <98967026+mcdermottm97@users.noreply.github.com>
2022-04-05 14:39:04 +0100

    implemented wrap-around

--- GameOfLifeP5/sketch.js
+++ GameOfLifeP5/sketch.js
@@ -16,12 +16,9 @@ let res = 10;
 
 
 function setup() {
-
   createCanvas(600, 600);
-
   cols = width / res;
   rows = height / res
-
   grid = make2DArray(cols, rows);
 
   for (let i = 0; i < cols; i++) {
@@ -29,28 +26,21 @@ function setup() {
       grid[i][j] = floor(random(2));
     }
   }
-
 }
 
 
 function draw() {
-
   background(0);
 
   for (let i = 0; i < cols; i++) {
     for (let j = 0; j < rows; j++) {
-      
       let x = i * res;
       let y = j * res;
 
       if (grid[i][j] == 1){
-
         fill(255);
-
         rect(x, y, res, res);
-
       }
-
     }
   }
 
@@ -59,37 +49,30 @@ function draw() {
   // count neighbors
   for (let i = 0; i < cols; i++) {
     for (let j = 0; j < rows; j++) {
-
       let state = grid[i][j]; 
+      let sum = 0;
+      let neighbors = countNeighbors(grid, i, j)      
 
-      if (i == 0 || i == cols - 1 || j == 0 || j == rows - 1) {
-        next[i][j] = state;
+      if (state == 0 && neighbors == 3) {
+        next[i][j] = 1;
+      } else if (state == 1 && (neighbors < 2 || neighbors > 3)) {
+        next[i][j] = 0;
       } else {
-
-        let sum = 0;
-        let neighbors = countNeighbors(grid, i, j)      
-
-        if (state == 0 && neighbors == 3) {
-          next[i][j] = 1;
-        } else if (state == 1 && (neighbors < 2 || neighbors > 3)) {
-          next[i][j] = 0;
-        } else {
-          next[i][j] = state;
-        }
+        next[i][j] = state;
       }
-
     }
   }
-
   grid = next;
-
 }
 
 function countNeighbors(grid, x, y) {
   let sum = 0;
   for (let i = -1; i < 2; i++) {
     for (let j = -1; j < 2; j++) {
-      sum += grid[x+i][y+j];
+      // wrap around 
+      let col = (x + i + cols) % cols;
+      let row = (y + j + rows) % rows;
+      sum += grid[col][row];
     }
   }
   sum -= grid[x][y];
