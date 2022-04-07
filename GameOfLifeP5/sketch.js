73e21a1e0cfb33ea16d5b3ffc1ab50f350c04ed2
mcdermottm97 <98967026+mcdermottm97@users.noreply.github.com>
2022-04-07 16:01:21 +0100

    Created a langtons ant simulator
    simulated a single virtual ant in p5.js

--- GameOfLifeP5/sketch.js
+++ GameOfLifeP5/sketch.js
@@ -1,79 +1,107 @@
-// Mathew McDermott
-// https://github.com/mcdermottm97/Cellular-Automata-Virtual-Ants.git
+// Matthew McDermott - 16032635
+// based off a CodingTrain challenge on Langtons ants
 
+
+// instantiate global variables for the grid and the ant
+let grid;
+let x;
+let y;
+let dir;
+
+// set global variables for direction
+let up = 0;
+let right = 1;
+let down = 2;
+let left = 3;
+
+// create canvas, create grid, and set initial ant position and direction
+function setup(){
+  createCanvas(600,600);
+  grid = make2DArray(width, height);
+  x = width/2;
+  y = height/2;
+  dir = up;
+}
+
+// main draw funciton 
+function draw() {
+  strokeWeight(1);
+  // this loop processes multiple steps for each rendered frame.
+  // essentially speed-multiplier at the cost of missed frames.
+  for (let n = 0; n < 100; n++) {
+    let state = grid[x][y];
+    if (state == 0) {
+      turnRight();
+      grid[x][y] = 1;
+    } else if (state == 1) {
+      turnLeft();
+      grid[x][y] = 0;
+    }
+
+    stroke(color(255));
+    if (grid[x][y] == 1) {
+      stroke(color(0));
+    }
+    point(x, y);
+    moveForward();
+  }
+
+}
+
+// function that returns a 2D array populated with 0 integers (white cells)
 function make2DArray(cols, rows) {
   let arr = new Array(cols);
   for (let i = 0; i < arr.length; i++){
     arr[i] = new Array(rows);
+    for (let j = 0; j < arr[i].length; j++) {
+      arr[i][j] = 0;
+    }
   }
   return arr;
 }
 
-let grid;
-let cols;
-let rows;
-let res = 10;
-
-
-function setup() {
-  createCanvas(600, 600);
-  cols = width / res;
-  rows = height / res
-  grid = make2DArray(cols, rows);
-
-  for (let i = 0; i < cols; i++) {
-    for (let j = 0; j < rows; j++) {
-      grid[i][j] = floor(random(2));
-    }
+// funciton to rotate ant +90 degrees
+function turnRight() {
+  dir++;
+  // using ascending integers for direction means wrapping back round requires selection
+  if (dir > left) {
+    dir = up
   }
 }
 
-
-function draw() {
-  background(0);
-
-  for (let i = 0; i < cols; i++) {
-    for (let j = 0; j < rows; j++) {
-      let x = i * res;
-      let y = j * res;
-
-      if (grid[i][j] == 1){
-        fill(255);
-        rect(x, y, res, res);
-      }
-    }
+// funciton to rotate ant -90 degrees
+function turnLeft() {
+  dir --;
+  // using ascending integers for direction means wrapping back round requires selection
+  if(dir < up) {
+    dir = left;
   }
-
-  let next = make2DArray(cols, rows);
-
-  for (let i = 0; i < cols; i++) {
-    for (let j = 0; j < rows; j++) {
-      let state = grid[i][j]; 
-      let sum = 0;
-      let neighbors = countNeighbors(grid, i, j)      
-
-      if (state == 0 && neighbors == 3) {
-        next[i][j] = 1;
-      } else if (state == 1 && (neighbors < 2 || neighbors > 3)) {
-        next[i][j] = 0;
-      } else {
-        next[i][j] = state;
-      }
-    }
-  }
-  grid = next;
 }
 
-function countNeighbors(grid, x, y) {
-  let sum = 0;
-  for (let i = -1; i < 2; i++) {
-    for (let j = -1; j < 2; j++) {
-      // wrap around 
-      let col = (x + i + cols) % cols;
-      let row = (y + j + rows) % rows;
-      sum += grid[col][row];
-    }
+// function to move ant forward in its current direction
+function moveForward() {
+  if (dir == up) {
+    y--;
+  } else if (dir == right) {
+    x++;
+  } else if (dir == down) {
+    y++;
+  } else if (dir == left) {
+    x--;
   }
-  sum -= grid[x][y];
-  return sum;
+
+  // controls wraparound on x axis
+  if (x > width - 1) {
+    x = 0;
+  } else if (x < 0) {
+    x = width - 1;
+  }
+
+  // controls wraparound on y axis
+  if (y > height -1) {
+    y = 0;
+  } else if (y < 0) { 
+    y = height - 1;
+  }
+
 }
